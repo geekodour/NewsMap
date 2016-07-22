04210045b29df34a864454fb15dcf6a2af2a23d5
geekodour <geekodour@go.ho>
2016-07-22 15:50:19 -0500

    moving

--- app/components/About.js
+++ app/components/About.js
@@ -2,7 +2,17 @@ var React = require('react');
 
 function about(props){
 	return (
-			<h1> About us Page </h1>
+		<div className="ui raised center aligned segment">
+			<h2 className="ui center aligned header">
+				About	
+			</h2>
+			<div style={{"width":"300px","margin":"auto"}}>
+			This is my first react app that I am putting online. There are many mistakes.
+			I'll keep making more apps like this. I also like making mall tools in bash. 
+			I'll upload them on GitHub too. 
+			Thankyou. You can find me on <a href="https://github.com/geekodour">Github.</a>
+			</div>
+		</div>
 		)
 }
 
--- app/components/HowWork.js
+++ app/components/HowWork.js
@@ -2,7 +2,21 @@ var React = require('react');
 
 function how(props){
 	return (
-			<h1> How us Page </h1>
+		<div className="ui raised center aligned segment">
+			<h2 className="ui center aligned header">
+				How NewsMap Works?
+			</h2>
+			<div style={{"width":"300px","margin":"auto"}}>
+			This uses Google news for fetching news for each country 
+			and then uses <a href="https://github.com/nlp-compromise/nlp_compromise">nlp compromise</a> for getting nouns,verbs and adjectives
+			out of the 10 news headlines fetched then I used a list that I combined from two sources and given words some score according 
+			to the positivity and negativity of the word where the score was not present.
+
+			Finally used Google GeoCharts to plot the score in the worldmap. 
+
+			It is made using react and the source code can be found <a href="https://github.com/geekodour/newsmap-react">here.</a>
+			</div>
+		</div>
 		)
 }
 
