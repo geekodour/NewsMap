88091e4b08c092f14bbe7792e7cbd6367658e5a5
geekodour <hrishikeshbman@gmail.com>
2016-07-26 16:46:12 +0530

    analytic added. hope for the best

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,15 +8,17 @@ var Home = React.createClass({
         return (
                 <div>
                 <div className="ui segment">
+		<h3>Note to note:</h3>
                 <ul>
-               	<li>This is a newsMap, uses very basic sentiment analysis to 
-               	detect Happiness of different countries.</li>
+               	<li>This is a NewsMap, uses very basic sentiment analysis to 
+               	detect <b>Happiness of different countries.</b></li>
                 <li><b>Usually takes maximum 30 seconds to load on slower connections.</b>
                	Please send bug <a href="https://github.com/geekodour/newsmap-react/issues">
-               	issues here</a> and No, it is
+               	issues here and please help with the bugs if you have free time</a> and No, it is
                	not responsive. </li>
                 <li>Extremely Sorry Firefox users, the app works 90% on Firefox but has 
-                <a href="https://github.com/geekodour/newsmap-react/issues/5"> some issues.</a> (click continue loading script if prompted) </li>
+                <a href="https://github.com/geekodour/newsmap-react/issues/5"> some issues.</a> (click continue loading script if prompted) Works fine on Chrome </li>
+		<li> My first react app ^_^ </li>
                 </ul>
                	</div>
                 <MapContainer/>
